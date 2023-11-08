05ea66212ae026fb9b9e24d9f4b088f5944eb58e
Mohamed A. Heiba <mohamedhieba877@gmail.com>
2023-11-08 02:59:22 +0200

    Finished browse screen.

--- lib/data/repos/movies_repo/data_sources/online_data_sources.dart
+++ lib/data/repos/movies_repo/data_sources/online_data_sources.dart
@@ -11,15 +11,15 @@ import '../../../model/similar_movies_responses.dart';
 import '../../../model/upcoming_movies_responses.dart';
 
 class OnlineDataSources {
-  static const String urlBase = "api.themoviedb.org";
+  static const String baseUrl = "api.themoviedb.org";
   static const String simiBaseUrl = "api.themoviedb.org/3/movie/";
-  static const String apiKey = "8bcaebf0d1310c08bc2f916753bff65e";
   static const String popularMoviesEndPoint = "/3/movie/popular";
   static const String upcomingMoviesEndPoint = "/3/movie/upcoming";
   static const String recommendedMoviesEndPoint = "/3/movie/top_rated";
   static const String detailsScreenEndPoint = "/3/movie/";
   static const String similarMoviesEndPoint = "/similar";
   static const String searchEndPoint = "/3/search/movie";
+  static const String genresListEndPoint = "/3/genre/movie/list";
   static const String discoverEndPoint = "/3/discover/movie";
 
   static const Map<String, String> headers = {
@@ -29,7 +29,7 @@ class OnlineDataSources {
   };
 
   Future<PopularMoviesResponses> getPopularMovies() async {
-    Uri url = Uri.https(urlBase, popularMoviesEndPoint);
+    Uri url = Uri.https(baseUrl, popularMoviesEndPoint);
     Response response = await http.get(url, headers: headers);
     Map json = jsonDecode(response.body);
     PopularMoviesResponses popularMoviesResponses =
@@ -43,7 +43,7 @@ class OnlineDataSources {
   }
 
   static Future<List<ResultsUp>> getUpcomingMovies() async {
-    Uri url = Uri.https(urlBase, upcomingMoviesEndPoint);
+    Uri url = Uri.https(baseUrl, upcomingMoviesEndPoint);
     Response response = await http.get(url, headers: headers);
     Map json = jsonDecode(response.body);
     UpcomingMoviesResponses upcomingMoviesResponses =
@@ -57,7 +57,7 @@ class OnlineDataSources {
   }
 
   static Future<List<ResultsRec>> getRecommendedMovies() async {
-    Uri url = Uri.https(urlBase, recommendedMoviesEndPoint);
+    Uri url = Uri.https(baseUrl, recommendedMoviesEndPoint);
     Response response = await http.get(url, headers: headers);
     Map json = jsonDecode(response.body);
     RecommendedMoviesResponses recommendedMoviesResponses =
@@ -71,7 +71,7 @@ class OnlineDataSources {
   }
 
   static Future<DetailsMovieResponses> getDetailsMovies(String id) async {
-    Uri url = Uri.parse("https://$urlBase$detailsScreenEndPoint$id");
+    Uri url = Uri.parse("https://$baseUrl$detailsScreenEndPoint$id");
     Response response = await http.get(url, headers: headers);
     Map json = jsonDecode(response.body);
     DetailsMovieResponses detailsMovieResponses =
@@ -98,8 +98,22 @@ class OnlineDataSources {
     throw Exception("Something went wrong...!");
   }
 
+  static Future<List<MovieGenres>> getGenresList() async
+  {
+    Uri url = Uri.https(baseUrl, genresListEndPoint);
+    Response response = await http.get(url, headers: headers);
+    Map json = jsonDecode(response.body);
+    GenresMovieListResponses genresMovieListResponses = GenresMovieListResponses.fromJson(json);
+    if(response.statusCode >= 200 && response.statusCode < 300 &&
+        genresMovieListResponses.genres!.isNotEmpty == true)
+    {
+      return genresMovieListResponses.genres!;
+    }
+    throw "Something went wrong please try again later...!";
+  }
+
   static Future<List<Results>> getMoviesBySearch(String q) async {
-    Uri url = Uri.https(urlBase, searchEndPoint, {"query": q});
+    Uri url = Uri.https(baseUrl, searchEndPoint, {"query": q});
     Response response = await http.get(url, headers: headers);
     Map json = jsonDecode(response.body);
     MoviesBySearchResponses moviesBySearchResponses =
@@ -112,15 +126,15 @@ class OnlineDataSources {
     throw Exception("Something went wrong...!");
   }
 
-  static Future<List<ResultsDisc>> getMoviesBrowse(String q) async {
-    Uri url = Uri.https(urlBase, discoverEndPoint, {"query": q});
+  static Future<List<ResultsDisc>> getMoviesBrowse(String id) async {
+    Uri url = Uri.https(baseUrl, discoverEndPoint, {"with_genres": id});
     Response response = await http.get(url, headers: headers);
     Map json = jsonDecode(response.body);
     DiscoverMoviesResponses discoverMoviesResponses =
-        DiscoverMoviesResponses.fromJson(json);
+    DiscoverMoviesResponses.fromJson(json);
     if (response.statusCode >= 200 &&
         response.statusCode < 300 &&
-        q.isNotEmpty) {
+        id.isNotEmpty) {
       return discoverMoviesResponses.results!;
     }
     throw Exception("Something went wrong...!");
